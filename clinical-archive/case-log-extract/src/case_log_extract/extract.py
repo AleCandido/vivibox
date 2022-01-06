4432706785cf128f995e5a3820de7fbf5f45f92e
Alessandro Candido <candido.ale@gmail.com>
2022-01-06 01:42:06 +0100

    Add pandas processing

--- clinical-archive/case-log-extract/src/case_log_extract/extract.py
+++ clinical-archive/case-log-extract/src/case_log_extract/extract.py
@@ -1,10 +1,64 @@
+import re
+
 import pandas as pd
 
 
-def main(infile, outfile):
-    print(f"Input {infile}")
-    print(f"Output {outfile}")
+def validate(infile, outfile):
+    if not infile.exists():
+        raise FileNotFoundError("No input file")
+    if outfile.exists():
+        raise FileExistsError("Cowardly refusing to overwrite existing file")
 
-    pd.read_excel(infile)
 
-    print("ciao")
+def load(infile):
+    return InputDataFrame(pd.read_excel(infile))
+
+
+def main(wrapdf, outfile):
+    wrapdf.drop_empty_rows()
+    wrapdf.drop_id1()
+    wrapdf.diagnosis_split_question()
+    wrapdf.diagnosis_normalize()
+    wrapdf.save(outfile)
+
+
+class InputDataFrame:
+    def __init__(self, df):
+        self.df = df
+
+    def drop_empty_rows(self):
+        self.df.dropna(how="all", inplace=True)
+
+    def drop_id1(self):
+        df = self.df
+        if "Patient ID.1" in df:
+            assert all(
+                (df["Patient ID"] == df["Patient ID.1"])
+                | ((df["Patient ID"].isna()) & df["Patient ID.1"].isna())
+            )
+            self.df.drop(columns=["Patient ID.1"], inplace=True)
+
+    def diagnosis_split_question(self):
+        """New field is not only true/false, but also which one, since multiple
+        diagnosis allowed, and not all of them have to be uncertain if one
+        is.
+        """
+        pass
+
+    def diagnosis_normalize(self):
+        diagnosis = self.df["Diagnosis"].apply(
+            lambda s: [
+                re.sub(r"\s+", " ", f.strip()).replace(" ", "_")
+                for f in s.lower().strip().split("_x001d_")
+            ]
+            if isinstance(s, str)
+            else []
+            if s != s
+            else (1 / 0, s)  # raise an error
+        )
+        self.df["Diagnosis"] = diagnosis.apply(lambda l: " ".join(l))
+
+    def save(self, out):
+        writer = pd.ExcelWriter(out)
+        self.df.to_excel(writer, "case log")
+        writer.save()
--- clinical-archive/case-log-extract/src/case_log_extract/ui.py
+++ clinical-archive/case-log-extract/src/case_log_extract/ui.py
@@ -6,6 +6,24 @@ from PySide6 import QtCore, QtGui, QtWidgets
 from . import extract
 
 
+class Extractor(QtCore.QObject):
+    validated = QtCore.Signal()
+    loaded = QtCore.Signal()
+    finished = QtCore.Signal(str, Exception)
+
+    def run(self, infile, outfile):
+        """Long-running task."""
+        try:
+            extract.validate(infile, outfile)
+            self.validated.emit()
+            wrapdf = extract.load(infile)
+            self.loaded.emit()
+            extract.main(wrapdf, outfile)
+            self.finished.emit("success", None)
+        except Exception as e:
+            self.finished.emit("failure", e)
+
+
 class FilePicker(QtWidgets.QWidget):
     def __init__(self, lab="File", default=""):
         super().__init__()
@@ -34,6 +52,9 @@ class FilePicker(QtWidgets.QWidget):
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
         self.setWindowTitle("Case Extract")
 
         self.inpicker = FilePicker("Input")
@@ -42,21 +63,50 @@ class MainWindow(QtWidgets.QWidget):
         )
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.button = QtWidgets.QPushButton("Extract")
+
+        self.botrow = QtWidgets.QHBoxLayout()
+        self.status = QtWidgets.QLabel("")
+        self.extr_button = QtWidgets.QPushButton("Extract")
+        self.extr_button.clicked.connect(self.extract)
+        self.botrow.addWidget(QtWidgets.QLabel())
+        self.botrow.addWidget(self.status, alignment=QtCore.Qt.AlignCenter)
+        self.botrow.addWidget(self.extr_button, alignment=QtCore.Qt.AlignRight)
 
         self.layout.addWidget(self.inpicker)
         self.layout.addWidget(self.outpicker)
-        self.layout.addWidget(self.button, alignment=QtCore.Qt.AlignRight)
-        #  self.button.resize(1, self.button.height())
-
-        self.button.clicked.connect(self.extract)
+        self.layout.addLayout(self.botrow)
 
     def extract(self):
-        try:
-            extract.main(self.inpicker.path(), self.outpicker.path())
-            self.success()
-        except Exception as e:
-            self.failure(e)
+        self.worker = Extractor()
+        self.thread = QtCore.QThread()
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(
+            lambda: self.worker.run(self.inpicker.path(), self.outpicker.path())
+        )
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+
+        self.worker.validated.connect(lambda: self.status.setText("validated"))
+        self.worker.loaded.connect(lambda: self.status.setText("loaded"))
+        self.extr_button.setEnabled(False)
+        self.thread.finished.connect(lambda: self.extr_button.setEnabled(True))
+        self.thread.finished.connect(lambda: self.status.setText(""))
+
+        def notify(res, exc):
+            if res == "success":
+                self.success()
+            else:
+                try:
+                    assert res == "failure"
+                except AssertionError as e:
+                    exc = e
+                    exc.args = (res,)
+                finally:
+                    self.failure(exc)
+
+        self.worker.finished.connect(lambda r, e: notify(r, e))
 
     def success(self):
         dialog = QtWidgets.QDialog(self)
@@ -91,21 +141,20 @@ class MainWindow(QtWidgets.QWidget):
         title = "<strong>Error..</strong>"
         subtitle = "Something has gone wrong..."
         _tb = "".join(traceback.format_tb(exc.__traceback__))
-        details = f"{exc.__class__.__name__}: {exc}\n\n{_tb}"
-        dialog.setText(f"{title}<br>{subtitle}")
-        dialog.setDetailedText(details)
-        dialog.setStyleSheet(
-            """QTextEdit {
-                font-family: monospace;
-                min-width: 600px;
-                min-height: 150px;
-            }"""
-        )
+        if isinstance(exc, FileExistsError):
+            pass
+        else:
+            details = f"{exc.__class__.__name__}: {exc}\n\n{_tb}"
+            dialog.setText(f"{title}<br>{subtitle}")
+            dialog.setDetailedText(details)
+            dialog.setStyleSheet(
+                """QTextEdit {
+                    font-family: monospace;
+                    min-width: 600px;
+                    min-height: 150px;
+                }"""
+            )
 
         dialog.resize(300, dialog.height())
-
-        #  layout = QtWidgets.QVBoxLayout(dialog)
-        #  layout.addWidget(QtWidgets.QLabel())
-        #  layout.addWidget(details)
 
         dialog.show()
